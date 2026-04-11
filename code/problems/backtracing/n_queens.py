438f5963f0d8fc4ddf8842ca58bee31da6b9d544
milly <16jamill16@gmail.com>
2026-04-11 00:01:34 -0600

    Start n queens problem

---
+++ code/problems/backtracing/n_queens.py
@@ -0,0 +1,12 @@
+# The N–queens puzzle is the problem of placing N chess queens on an N × N chessboard so that no two queens threaten each other.
+# Thus, the solution requires that no two queens share the same row, column, or diagonal.
+
+# For example, for a standard 8 × 8 chessboard, below is one such configuration:
+# Q  –  –  –  –  –  –  – 
+# –  –  –  –  Q  –  –  – 
+# –  –  –  –  –  –  –  Q 
+# –  –  –  –  –  Q  –  – 
+# –  –  Q  –  –  –  –  – 
+# –  –  –  –  –  –  Q  – 
+# –  Q  –  –  –  –  –  – 
+# –  –  –  Q  –  –  –  –
